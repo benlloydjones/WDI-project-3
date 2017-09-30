2be6e23044f5e214e3c04c564eafa0888a48b44c
savvala <savva.l@hotmail.com>
2017-09-30 14:27:47 +0100

    Removing unneeded second proxy controller

--- controllers/googlePlaces.js
+++ controllers/googlePlaces.js
@@ -2,15 +2,14 @@ const rp = require('request-promise');
 
 function googlePlacesProxy(req, res) {
   rp({
-    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=51.5363321,-0.1031599&radius=500&type=bar&key=AIzaSyDHSfv1q1Iy7wE4b_dncrPpXwsKFqrJkh8',
+    url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
     method: 'GET',
     json: true,
     qs: {
-      api_key: process.env.GOOGLE_PLACES_API_KEY,
-      latitude: req.query.lat,
-      longitude: req.query.lng,
-      radius: 5,
-      eventcode: req.query.eventcode
+      key: process.env.GOOGLE_PLACES_API_KEY,
+      location: `${req.query.lat},${req.query.lng}`,
+      radius: 500,
+      type: req.query.type
     }
   })
     .then((response) => res.json(response))
